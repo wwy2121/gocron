e6e834bdc7c786f592319ff8ad4f35b62beb98a1
wenying.wu <wenying.wu@baisonmai.com>
2020-05-27 11:06:40 +0800

    etcd test

--- etcd/demo1/main.go
+++ etcd/demo1/main.go
@@ -9,33 +9,35 @@ import (
 
 func main() {
 	var (
-		config clientv3.Config
-		client *clientv3.Client
-		kv clientv3.KV
+		config  clientv3.Config
+		client  *clientv3.Client
+		kv      clientv3.KV
 		putResp *clientv3.PutResponse
 		getResp *clientv3.GetResponse
-		err error
+		ctx     context.Context
+		err     error
 	)
 	config = clientv3.Config{
-		Endpoints:[]string{"192.168.99.115:2379"},
-		DialTimeout:5*time.Second,
+		Endpoints:   []string{"192.168.99.100:2379"},
+		DialTimeout: 5 * time.Second,
 	}
-	client,err = clientv3.New(config)
-	if err != nil{
+	client, err = clientv3.New(config)
+	if err != nil {
 		fmt.Print(err)
 		return
 	}
 	kv = clientv3.NewKV(client)
+	ctx, _ = context.WithTimeout(context.TODO(), 5*time.Second)
 
 	//put
-	putResp,err = kv.Put(context.TODO(),"/cron/a","123",clientv3.WithPrevKV())
+	putResp, err = kv.Put(ctx, "/cron/a", "123", clientv3.WithPrevKV())
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Println(putResp.Header.Revision)
 
 	//get
-	getResp,err = kv.Get(context.TODO(), "/cron/a",clientv3.)
+	getResp, err = kv.Get(ctx, "/cron/a", clientv3.WithPrefix())
 	if err != nil {
 		fmt.Print(err)
 	}
